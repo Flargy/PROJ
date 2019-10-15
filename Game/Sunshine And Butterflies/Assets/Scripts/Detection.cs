1edd9f0ec1af15ea2796318580cf2d4c881fcc89
LouisXcypheR <fquinterosureta@gmail.com>
2019-10-15 11:10:01 +0200

    Surveillence camera with detection script
    Övervakninskamera med rotations script samt detektion script.

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/Detection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask detectionLayer;
+    Transform lens;
+    void Start()
+    {
+        lens = transform.parent.GetComponent<Transform>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("inside field");
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Vector3 direction = other.transform.position - lens.position;
+            RaycastHit hit;
+
+            if(Physics.Raycast(lens.transform.position, direction.normalized, out hit, 1000, detectionLayer))
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("test");
+        }
+    }
+}
