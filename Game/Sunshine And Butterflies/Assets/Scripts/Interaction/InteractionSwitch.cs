0d370e4ff1ce8b8f91d93e1d557eec04b335a687
LouisXcypheR <fquinterosureta@gmail.com>
2019-10-15 13:54:09 +0200

    Basen för InteractionSwitch
    Bas för interactionSwitch skriptet

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/Interaction/InteractionSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSwitch : MonoBehaviour
+{
+    [SerializeField] private AffectedObject firstAffectedObject;
+    [SerializeField] private AffectedObject otherAffectedObject;
+
+
+    //interact funktion()
+
+    //avaktivera objekt
+    //aktivera det andra objektet
+    // iterera
+
+}
