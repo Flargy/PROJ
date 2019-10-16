389580435eace72c1937ef65315a02b02a3bf525
Flargy <marlun1996@hotmail.com>
2019-10-16 14:26:49 +0200

    new input system

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/Input/PlayerController.cs
@@ -0,0 +1,190 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/PlayerController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class PlayerController : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public PlayerController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerController"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""5674b4e8-1d73-480b-a0ea-494e4ded920b"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""c5108b8a-26aa-4fc4-92c6-477325b009cb"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""bad24792-e118-435b-af34-155a10cb74ab"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""b03d1bb4-8012-4927-86c3-c2361782eddb"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""dd976ea9-8b81-4eae-a2fd-6d864cdcb373"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4e704ff1-1967-429f-b5eb-51ae26cf8589"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0557ff5a-6fe0-4811-a8a4-d759cf87fc30"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
+        m_Gameplay_Interact = m_Gameplay.FindAction("Interact", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_Jump;
+    private readonly InputAction m_Gameplay_Interact;
+    public struct GameplayActions
+    {
+        private PlayerController m_Wrapper;
+        public GameplayActions(PlayerController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
+        public InputAction @Interact => m_Wrapper.m_Gameplay_Interact;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                Interact.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnInteract;
+                Interact.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnInteract;
+                Interact.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnInteract;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Move.started += instance.OnMove;
+                Move.performed += instance.OnMove;
+                Move.canceled += instance.OnMove;
+                Jump.started += instance.OnJump;
+                Jump.performed += instance.OnJump;
+                Jump.canceled += instance.OnJump;
+                Interact.started += instance.OnInteract;
+                Interact.performed += instance.OnInteract;
+                Interact.canceled += instance.OnInteract;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnInteract(InputAction.CallbackContext context);
+    }
+}
---
+++ Game/Sunshine And Butterflies/Assets/Scripts/NewPlayerScript.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewPlayerScript : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask interactionLayer;
+    [SerializeField] private float moveSpeed = 6.0f;
+    [SerializeField] private float jumpPower = 5.0f;
+
+    private Rigidbody rb = null;
+    private Vector2 movementVector = Vector2.zero;
+    private bool interacting = false;
+    private bool carryingABox = false;
+    private bool airBorne = false;
+    private GameObject carriedBox;
+    private float jumpGroundCheckDelay = 0.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(airBorne == false && interacting == false)
+        {
+            ApplyGroundVelocity();
+            TurnToFace();
+        }
+        else if(airBorne == true)
+        {
+            jumpGroundCheckDelay += Time.deltaTime;
+            ApplyAirVelocity();
+            if(GroundCheck() == true && jumpGroundCheckDelay >= 0.3f)
+            {
+                airBorne = false;
+                jumpGroundCheckDelay = 0.0f;
+            }
+        }
+    }
+
+    public void TurnToFace()
+    {
+        Vector3 movementVector = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (movementVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movementVector);
+        }
+    }
+
+    public void OnInteract()
+    {
+        RaycastHit ray;
+        if (carryingABox == false) // placeholder
+        {
+
+            if (Physics.Raycast(transform.position, transform.forward, out ray, 2.0f, interactionLayer))
+            {
+                if (ray.collider.CompareTag("CarryBox"))
+                {
+                    carriedBox = ray.collider.gameObject;
+                    Interactable interactionObject = ray.collider.GetComponent<Interactable>();
+                    interactionObject.Interact(gameObject);
+                    carryingABox = true;
+                    Debug.Log(carryingABox);
+                }
+                else
+                {
+                    Interactable interactionObject = ray.collider.GetComponent<Interactable>();
+                    interactionObject.DistanceCheck(transform.position);
+                }
+            }
+        }
+
+        else if (carryingABox == true) //Ska brytas ut till egen state
+        {
+
+            carriedBox.GetComponent<InteractionPickUp>().Drop();
+            carryingABox = false;
+        }
+    }
+
+    public void ApplyGroundVelocity()
+    {
+        rb.velocity = (new Vector3(movementVector.x, 0, movementVector.y) * moveSpeed) + new Vector3(0, rb.velocity.y, 0);
+        if (new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude >= 2.51f)
+        {
+            float yVelocity = rb.velocity.y;
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, 2.5f);
+            rb.velocity = new Vector3(rb.velocity.x, yVelocity, rb.velocity.z);
+        }
+    }
+
+    public bool GroundCheck()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 0.8f, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void ApplyAirVelocity()
+    {
+        Vector2 currentVelocity = new Vector2(rb.velocity.x, rb.velocity.z);
+        float turndot = Vector2.Dot(currentVelocity, movementVector);
+        if (turndot < 0.3)
+        {
+            rb.velocity += (new Vector3(movementVector.x, 0, movementVector.y) * 0.05f);
+
+        }
+
+    }
+
+    public void OnMove(InputValue value)
+    {
+        movementVector = value.Get<Vector2>();
+    }
+
+    public void OnJump()
+    {
+        if (GroundCheck() == true && interacting == false)
+        {
+            rb.velocity += Vector3.up * jumpPower;
+            airBorne = true;
+        }
+    }
+}
