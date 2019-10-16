4c9e494b1aea3d27f56ba8fedb25e5652ff99f35
LouisXcypheR <fquinterosureta@gmail.com>
2019-10-17 00:40:27 +0200

    Death Traps
    Arrow Trap and trapdoor
    
    Arrows rotation at the instantiate has to be fixed

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/Arrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    public float arrowVelocity;
+    public float arroLife = 2.0f;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = transform.forward * arrowVelocity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer <= arroLife)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Game/Sunshine And Butterflies/Assets/Scripts/ArrowTrap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+
+    GameObject[] arrowSpawns;
+    public GameObject arrow;
+
+    int arrowNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrowSpawns = GameObject.FindGameObjectsWithTag("ArrowSpawn");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        arrowNum = Random.Range(1, arrowSpawns.Length);
+
+        Debug.Log("Collide");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            for(int i = 0; i < arrowNum; i++)
+            {
+                GameObject arrowPos = arrowSpawns[Random.Range(0, arrowSpawns.Length)];
+                Instantiate(arrow, arrowPos.transform.position, arrowPos.transform.rotation);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
