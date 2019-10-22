a9e5a34f9914be2c312932a78b6f273677fd6094
LouisXcypheR <fquinterosureta@gmail.com>
2019-10-22 21:48:44 +0200

    SpikeWall
    Spike wall

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/SpikeTrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(Go());
+    }
+
+    IEnumerator Go()
+    {
+        while (true)
+        {
+            gameObject.GetComponent<Animation>().Play();
+            yield return new WaitForSeconds(3f);
+        }
+    }
+    
+}
