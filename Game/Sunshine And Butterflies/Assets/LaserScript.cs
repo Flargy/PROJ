9d35bf35f4402bbe044e06f7e3dbf48c2228e801
LouisXcypheR <fquinterosureta@gmail.com>
2019-10-24 18:47:35 +0200

    LT toggle on/off and position fix

--- Game/Sunshine And Butterflies/Assets/LaserScript.cs
+++ Game/Sunshine And Butterflies/Assets/LaserScript.cs
@@ -8,17 +8,36 @@ public class LaserScript : MonoBehaviour
     public Transform startPoint;
     public Transform endPoint;
     LineRenderer laserLine;
+
+    public GameObject Laser;
+    public bool trapEnable = true;
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("ToggleLaserTrap", 0, 5f);
         laserLine = GetComponent<LineRenderer>();
-       //laserLine.SetWidth(.2f, .2f);
+       laserLine.SetWidth(.2f, .2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        laserLine.useWorldSpace = true;
         laserLine.SetPosition(0, startPoint.position);
         laserLine.SetPosition(1, endPoint.position);
+    }
+
+    private void ToggleLaserTrap()
+    {
+        trapEnable = !trapEnable;
+        Laser.SetActive(trapEnable);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Dead");
+        }
     }
 }
