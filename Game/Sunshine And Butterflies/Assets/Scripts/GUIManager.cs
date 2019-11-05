b2ec7ff3fb311c305076ab70f2b8770e51da5fb1
LouisXcypheR <fquinterosureta@gmail.com>
2019-11-05 19:43:54 +0100

    MenuTest Input config

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+
+    public void Play()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+}
