832dd332339b947afe469d2b65f0f4930ebe9f99
Flargy <marlun1996@hotmail.com>
2019-11-07 15:06:44 +0100

    stuff

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/DoorLightChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLightChange : MonoBehaviour
+{
+    [SerializeField] private Color activationColor;
+
+    private Renderer lightRenderer = null;
+    private MaterialPropertyBlock propertyBlock = null;
+
+
+    private void Start()
+    {
+        lightRenderer = GetComponent<Renderer>();
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    public void ChangeEmission(float strength)
+    {
+        Debug.Log(strength);
+        lightRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor("_EmissionColor", activationColor * (strength * 3));
+        lightRenderer.SetPropertyBlock(propertyBlock);
+
+    }
+
+   
+}
---
+++ Game/Sunshine And Butterflies/Assets/Scripts/PropertyBlockTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyBlockTest : Interactable
+{
+    public Color swapToColor;
+
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    public override void Interact(GameObject player)
+    {
+        Debug.Log("activated");
+        ChangeColor();
+    }
+
+    private void Start()
+    {
+        _propBlock = new MaterialPropertyBlock();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void ChangeColor()
+    {
+        _renderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetColor("_EmissionColor", swapToColor);
+        //_propBlock.SetColor("_BaseColor", swapToColor);
+        _renderer.SetPropertyBlock(_propBlock);
+
+    }
+}
