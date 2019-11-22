3207e75e2b4a49e081e9e63d7e7c7bdfe19321f3
LouisXcypheR <fquinterosureta@gmail.com>
2019-11-22 10:58:25 +0100

    AudioTrigger
    .

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip triggerSound;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            audioSource.PlayOneShot(triggerSound);
+        }
+    }
+
+}
