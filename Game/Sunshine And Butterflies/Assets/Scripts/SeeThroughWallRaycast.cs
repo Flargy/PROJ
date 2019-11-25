0085dd60c22074c3c2fa75a7f2b0cd0f6d0237e5
Flargy <marlun1996@hotmail.com>
2019-11-25 11:07:23 +0100

    debug information

--- Game/Sunshine And Butterflies/Assets/Scripts/SeeThroughWallRaycast.cs
+++ Game/Sunshine And Butterflies/Assets/Scripts/SeeThroughWallRaycast.cs
@@ -7,14 +7,20 @@ public class SeeThroughWallRaycast : MonoBehaviour
     [SerializeField] private GameObject seeThrough = null;
     [SerializeField] private LayerMask wallLayer = 0;
     [SerializeField] private GameObject cam = null;
+    [SerializeField] private float radius = 1.0f;
 
 
     private void FixedUpdate()
     {
-        Debug.DrawRay(transform.position + Vector3.up * 0.6f, cam.transform.position - transform.position);
+        Debug.DrawRay(transform.position + Vector3.up * 0.6f, cam.transform.position - transform.position + Vector3.up * 0.6f);
+        Debug.DrawRay(transform.position + Vector3.up * 0.6f + Vector3.right * radius, cam.transform.position - transform.position + Vector3.up * 0.6f);
+        Debug.DrawRay(transform.position + Vector3.up * 0.6f + Vector3.left * radius, cam.transform.position - transform.position + Vector3.up * 0.6f);
+        Debug.DrawRay(transform.position + Vector3.up * 0.6f + Vector3.up * radius, cam.transform.position - transform.position + Vector3.up * 0.6f);
+        Debug.DrawRay(transform.position + Vector3.up * 0.6f + Vector3.down * radius, cam.transform.position - transform.position + Vector3.up * 0.6f);
+
 
         RaycastHit hit;
-        if(Physics.SphereCast(transform.position + Vector3.up * 0.6f, 1.5f, cam.transform.position - (transform.position + Vector3.up * 0.6f), out hit, 10.0f, wallLayer))
+        if(Physics.SphereCast(transform.position + Vector3.up * 0.6f, radius, cam.transform.position - (transform.position + Vector3.up * 0.6f), out hit, 10.0f, wallLayer))
         {
             seeThrough.transform.LookAt(transform.position - cam.transform.position, Vector3.up);
             if (seeThrough.activeSelf == false)
@@ -29,5 +35,11 @@ public class SeeThroughWallRaycast : MonoBehaviour
                 seeThrough.SetActive(false);
             }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position + Vector3.up * 0.6f, radius);
     }
 }
