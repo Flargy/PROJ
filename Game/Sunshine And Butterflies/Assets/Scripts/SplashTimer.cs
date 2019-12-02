51d69180cc62e0c49a8e40f2b66378f669bdde93
EkuDeku <ekuman.k@gmail.com>
2019-12-02 12:27:07 +0100

    SplashScene + Splash Delay

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/SplashTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashTimer : MonoBehaviour
+{
+    [SerializeField] float timeDelayed = 0;
+
+    void Start()
+    {
+        StartCoroutine(SceneDelay());
+    }
+
+    private IEnumerator SceneDelay()
+    {
+        yield return new WaitForSeconds(timeDelayed);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+}
