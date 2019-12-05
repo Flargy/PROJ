1f6fdf384d27cbd2e404be0f039129792af12609
Flargy <marlun1996@hotmail.com>
2019-12-05 14:58:05 +0100

    box pickup raycast fix

--- Game/Sunshine And Butterflies/Assets/Scripts/Interaction/InteractionPickUp.cs
+++ Game/Sunshine And Butterflies/Assets/Scripts/Interaction/InteractionPickUp.cs
@@ -45,7 +45,7 @@ public class InteractionPickUp : Interactable
             {
                 RaycastHit plateHit;
                 //if (Physics.Raycast(transform.position + Vector3.up * 0.5f, Vector3.down, out plateHit, 1f, LayerMask.GetMask("PressurePlate")))
-                if(Physics.BoxCast(transform.position + Vector3.up * 0.5f, colliders[0].size/2, Vector3.down, out plateHit, Quaternion.identity, 1.0f, LayerMask.GetMask("PressurePlate") ))
+                if(Physics.BoxCast(transform.position + Vector3.up * 0.5f, colliders[0].size/2, Vector3.down, out plateHit, Quaternion.identity, 0.45f, LayerMask.GetMask("PressurePlate") ))
                 {
                     plateHit.collider.GetComponent<PressurePlate>().LowerCounter();
                 }
