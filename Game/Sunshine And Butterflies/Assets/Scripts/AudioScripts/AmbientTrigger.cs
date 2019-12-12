29a1348d7f4dbcbaa50a7887ac84bfcebc90e505
LouisXcypheR <fquinterosureta@gmail.com>
2019-12-12 23:16:33 +0100

    AmbientCrossfade funktionalitet.
    .

---
+++ Game/Sunshine And Butterflies/Assets/Scripts/AudioScripts/AmbientTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientTrigger : MonoBehaviour
+{
+    public AmbientSoundPlayer ambientSoundPlayer;
+    public int playersRequired = 2;
+    private List<GameObject> players = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !players.Contains(other.gameObject))
+        { 
+            players.Add(other.gameObject);
+            Debug.Log("Player entered:^" + other.gameObject.name + " count:^" + players.Count);
+        }
+
+        CheckSound();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && players.Contains(other.gameObject))
+        {
+            players.Remove(other.gameObject);
+            Debug.Log("Player escaped:^" + other.gameObject.name + " count:^" + players.Count);
+        }
+
+        CheckSound();
+    }
+
+    private void CheckSound() 
+    {
+        if (players.Count >= playersRequired)
+            ambientSoundPlayer.PlaySound2();
+        else
+            ambientSoundPlayer.PlaySound1();
+    }
+}
